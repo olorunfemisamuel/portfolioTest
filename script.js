2312451878db8137ab2a7bf645d85f7e2017d45b
Olorunfemi Samuel <forfemi@gmail.com>
2025-12-04 15:49:00 +0100

    init

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById('menu-btn');
+    const menu = document.getElementById('mobile-menu');
+
+    btn.addEventListener('click', () => {
+        menu.classList.toggle('hidden');
+    });
+});
