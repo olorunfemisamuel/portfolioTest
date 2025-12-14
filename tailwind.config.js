ab0960af33e74c74a91256de158efefc0cab1453
Olorunfemi Samuel <forfemi@gmail.com>
2025-12-14 07:21:55 +0100

    Added animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./index.html", "./*.html", "./js/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+    slideInUp: {
+      '0%': { transform: 'translateY(100%)', opacity: '0' },
+      '100%': { transform: 'translateY(0)', opacity: '1' },
+    },
+  },
+  animation: {
+    slideInUp: 'slideInUp 2s ease-out forwards',
+  },
+    },
   },
   plugins: [],
 }
+
+
+  
+
+
